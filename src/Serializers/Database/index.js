eadb70ddd6e49c60fcbcb4008191b8c5bc95f14e
Harminder Virk <virk.officials@gmail.com>
2016-04-15 18:47:54 +0530

    feat(serializer): initiate Database serializer

--- src/Serializers/Database/index.js
+++ src/Serializers/Database/index.js
@@ -8,3 +8,35 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
 */
+
+class DatabaseSerializer {
+
+  static get inject () {
+    return ['Adonis/Src/Database', 'Adonis/Src/Hash']
+  }
+
+  constructor (Database, Hash) {
+    this.database = Database
+    this.hash = Hash
+  }
+
+  * findById (id, options) {
+    return yield this.database.table(options.table).where('id', id).first()
+  }
+
+  * findByCredentials (email, constraints, options) {
+  }
+
+  * findByToken (token, options) {
+
+  }
+
+  * validateToken (token, options) {
+  }
+
+  * validateCredentials (user, credentials, options) {
+  }
+
+}
+
+module.exports = DatabaseSerializer
